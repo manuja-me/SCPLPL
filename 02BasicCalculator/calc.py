c2c0848dd25bbd6ad9309ed3f555d66e7d92d242
Manuja <manuja.public@gmail.com>
2024-07-25 13:40:02 +0530

    completed calc.py

---
+++ 02BasicCalculator/calc.py
@@ -0,0 +1,28 @@
+def calc(num1, num2, operator):
+    if operator == '+': 
+        return num1 + num2
+    elif operator == '-': 
+        return num1 - num2
+    elif operator == '*': 
+        return num1 * num2
+    elif operator == '/':
+        if num2 == 0:
+            return "division by zero error"
+        else:
+            return num1 / num2
+    elif operator == '^': 
+        return num1 ** num2
+    elif operator == '%': 
+        return num1 % num2
+    else: 
+        return "invalid operator"
+
+# Get input from the user:
+try:
+    num1 = float(input('Enter the first number: '))
+    operator = input('Enter the operator (+, -, /, *, %, ^): ')
+    num2 = float(input('Enter the second number: '))
+    result = calc(num1, num2, operator)
+    print('Answer:', result)
+except ValueError:
+    print("Invalid input. Please enter numeric values for numbers.")
