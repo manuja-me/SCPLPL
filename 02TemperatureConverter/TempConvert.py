125f6efb44c60b45c704d1058100caf086c1d007
Manuja <manuja.public@gmail.com>
2024-07-31 14:44:52 +0530

    .py refactered

--- 02TemperatureConverter/TempConvert.py
+++ 02TemperatureConverter/TempConvert.py
@@ -1,23 +1,11 @@
-def FtoC(num):
-    Celsius = (num - 32)*5/9
-    return Celsius
+method = input("to Fahrenheit(f) or Celsius(c): ")
+num = float(input("Enter a Value: "))
 
-def CtoF(num):
-    Fahrenheit = (num*9/5) + 32
-    return Fahrenheit
-
-def main():
-    num = float(input("Enter A Value: "))
-    method = input("to Fahrenheit(1) or Celsius(2): ")
-
-    if method == "1":
-        result = CtoF(num)
-    elif method == "2":
-        result = FtoC(num)
-    else:
-        result = "please select only 1 or 2"
-
-    return result
-
-result = main()
-print(result)
+if method == "f":
+    Fahrenheit = (num * 9 / 5) + 32
+    print(f"{Fahrenheit}")
+elif method == "c":
+    Celsius = (num - 32) * 5 / 9
+    print(f"{Celsius}")
+else:
+    print("Wrong Input")
