bf5f283fbf2700072599077e13f645ef6b28a4ae
Manuja <manuja.public@gmail.com>
2024-09-29 14:09:24 +0530

    this is a mess

--- Java/cInterpreter/lox/Lox.java
+++ Java/cInterpreter/lox/Lox.java
@@ -61,6 +61,14 @@ public class Lox {
     handError = true;
   }
 
+  static void error(Token token, String message) {
+    if (token.type == TokenType.EOF) {
+      report(token.line, " at end", message);
+    } else {
+      report(token.line, " at '" + token.lexeme + "'", message);
+    }
+  }
+
   static boolean handError = false;
 
 }
